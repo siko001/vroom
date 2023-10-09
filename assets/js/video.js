a0787ddcc1d0bbc8c020302fee94595df0584e0b
Neil <neil.mallia1@gmail.com>
2023-10-09 22:53:42 +0200

    final version

--- assets/js/video.js
+++ assets/js/video.js
@@ -20,25 +20,4 @@ document.addEventListener('DOMContentLoaded', function () {
 	function showPopup() {
 		shadowbox.classList.remove('vimeo-shadowbox--hidden');
 	}
-
-	fetch('https://vimeo.com/api/oembed.json?url=https://player.vimeo.com/video/871797777')
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error('Network response was not ok');
-			}
-			return response.json(); // Parse the JSON response
-		})
-		.then((data) => {
-			let videoHtml = data.html;
-
-			// Extract the src attribute from the video HTML
-			const parser = new DOMParser();
-			const doc = parser.parseFromString(videoHtml, 'text/html');
-			const iframeElement = doc.querySelector('iframe');
-			const iframeSrc = iframeElement.getAttribute('src');
-		})
-		.catch((error) => {
-			// Handle any errors that occurred during the fetch or processing of the response
-			console.error('There was a problem with the fetch operation:', error);
-		});
 });
--- functions.php
+++ functions.php
@@ -18,3 +18,9 @@ function blue_text_shortcode($atts, $content = null) {
     return '<span class="blue"><strong>' . $content . '</strong></span>';
 }
 add_shortcode('blue', 'blue_text_shortcode');
+
+
+function custom_login_message() {
+    echo '<h1>Welcome to the website. Please login with this account</h1> <br> <h3 style="color:red;">Username: james@gmail.com</h3><br><h3 style="color:red;">Password: LR@%#11m7gVMqY6DqWL9xZE9</h3><br><p>you can edit the front page and customise content through the theme customizer add more blogs, services or even rental cars! front page will update accordingly</p><br>';
+}
+add_action('login_message', 'custom_login_message');
